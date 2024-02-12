4e5b54057a5a0e8b5306719f31a75c93de99e0ae
EdvardGarmannslund <ged@ssb.no>
2024-02-12 15:44:36 +0100

    la till stubs

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,6 +2,7 @@
 
 import pytest
 from click.testing import CliRunner
+
 from ssb_konjunk import __main__
 
 
--- tests/test_prompts.py
+++ tests/test_prompts.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 import pytest
+
 from ssb_konjunk.prompts import days_in_month
 from ssb_konjunk.prompts import extract_start_end_dates
 from ssb_konjunk.prompts import next_month
