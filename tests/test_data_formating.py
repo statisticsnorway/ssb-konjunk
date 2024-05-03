16813d16630595ec2747b5e5a7d7184782dbdee0
EdvardGarmannslund <ged@ssb.no>
2024-05-03 10:22:23 +0200

    pre-commit format fiks

--- tests/test_data_formating.py
+++ tests/test_data_formating.py
@@ -1,6 +1,3 @@
-from datetime import datetime
-
-import pytest
 import pandas as pd
 
 from ssb_konjunk.data_formating import bytte_koder
@@ -9,20 +6,17 @@ from ssb_konjunk.data_formating import bytte_koder
 
 
 def test_bytte_koder() -> None:
-    kode_dict = {
-        "a":"1",
-        "b":"2"
-    }
-    
+    kode_dict = {"a": "1", "b": "2"}
+
     df = pd.DataFrame(
         {
-            "bokstaver":["a","b"],
-            "tall":[1,2],
+            "bokstaver": ["a", "b"],
+            "tall": [1, 2],
         }
     )
-    
-    df = bytte_koder(df,kode_dict,"bokstaver")
 
-    assert df["bokstaver"].to_list()==["1","2"]
-    
-    assert df["tall"].to_list() == [1,2]
+    df = bytte_koder(df, kode_dict, "bokstaver")
+
+    assert df["bokstaver"].to_list() == ["1", "2"]
+
+    assert df["tall"].to_list() == [1, 2]
