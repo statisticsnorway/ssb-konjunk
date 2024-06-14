c985099ee6f845deb86d4037b4835147c3a5cba8
EdvardGarmannslund <ged@ssb.no>
2024-06-14 16:54:42 +0200

    pre-commit fiks

--- src/ssb_konjunk/xml_handling.py
+++ src/ssb_konjunk/xml_handling.py
@@ -1,35 +1,33 @@
 """A collection of functions to make xml files handling easier both in dapla and prodsone.
 """
 
-import pandas as pd
 import xml.etree.ElementTree as ET
 
 
 def read_xml(xml_file: str, fs=None) -> ET.Element:
     """Funtion to get xml root from disk.
-    
+
     Args:
         xml_file: Strin value for xml filepath.
         fs: filesystem
-    
+
     Returns:
-        ET.Element: Root of xml file."""
-    
+    ET.Element: Root of xml file.
+    """
     if fs:
         with fs.open(xml_file, mode="r") as file:
             single_xml = file.read()
             file.close()
     else:
-        with open(xml_file, mode="r") as file:
+        with open(xml_file) as file:
             single_xml = file.read()
             file.close()
-            
+
     return ET.fromstring(single_xml)
 
 
-def return_txt_xml(root: ET.Element, child: str) -> str|None:
-    """
-    Function to return text value from child element in xml file.
+def return_txt_xml(root: ET.Element, child: str) -> str | None:
+    """Function to return text value from child element in xml file.
 
     Args:
         root: Root with all data stored in a branch like structure.
@@ -40,7 +38,3 @@ def return_txt_xml(root: ET.Element, child: str) -> str|None:
     """
     for element in root.iter(child):
         return element.text
-
-
-
-
