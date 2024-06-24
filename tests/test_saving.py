02575823c41ce63a04f9b8f5b4860314b79ad660
Johanne Saxegaard <jox@ssb.no>
2024-06-24 15:27:24 +0200

    testmaking

--- tests/test_saving.py
+++ tests/test_saving.py
@@ -5,13 +5,5 @@ from ssb_konjunk.saving import get_time_period_standard
 """Test function get_time_period_standard"""
 
 def test_get_time_period_standard() -> None:
-    # TEst
-    base_name = 'filnavn'
-    start_year = 2023
-    start_month = 1
-    start_day = 1
-    end_year = 2024
-    end_month = 2
-    end_day = 14
-    
+    # TEst    
     assert get_time_period_standard('filnavn', 2023, 1,22) == 'filnavn_p2023-01-22_v', f"get_time_period_standard('filnavn', 2023, 1,22)"
