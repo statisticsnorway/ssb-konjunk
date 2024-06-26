38eeda58e582c7b50e2e963d0f98ba01f2cb7967
Johanne Saxegaard <jox@ssb.no>
2024-06-26 10:12:18 +0200

    fix

--- tests/test_saving.py
+++ tests/test_saving.py
@@ -1,9 +1,10 @@
-import pytest
-
 from ssb_konjunk.saving import get_time_period_standard
 
 """Test function get_time_period_standard"""
 
+
 def test_get_time_period_standard() -> None:
-    # TEst    
-    assert get_time_period_standard('filnavn', 2023, 1,22) == 'filnavn_p2023-01-22_v', f"get_time_period_standard('filnavn', 2023, 1,22)"
+    # TEst
+    assert (
+        get_time_period_standard("filnavn", 2023, 1, 22) == "filnavn_p2023-01-22_v"
+    ), "get_time_period_standard('filnavn', 2023, 1,22)"
