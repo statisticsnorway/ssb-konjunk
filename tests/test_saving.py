88e6e505117f4e1084b3a8284390fbd97564fd7e
Johanne Saxegaard <jox@ssb.no>
2024-06-26 10:18:23 +0200

    fix

--- tests/test_saving.py
+++ tests/test_saving.py
@@ -10,4 +10,4 @@ def test_get_time_period_standard() -> None:
     # TEst
     assert (
         get_time_period_standard("filnavn", 2023, 1, 22) == "filnavn_p2023-01-22_v"
-    ), "get_time_period_standard('filnavn', 2023, 1,22)"
+    ), get_time_period_standard('filnavn', 2023, 1,22)
