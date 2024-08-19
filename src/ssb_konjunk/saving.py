621fcc9e293cc54280bb92b17a288ab541085b5c
Johanne Saxegaard <jox@ssb.no>
2024-08-19 13:16:45 +0200

    Ensure None is not in list of existing files

--- src/ssb_konjunk/saving.py
+++ src/ssb_konjunk/saving.py
@@ -100,7 +100,10 @@ def _get_files(folder_path: str, fs: dapla.gcs.GCSFileSystem | None) -> list[str
 
 def _find_version_number(files: list[str], stable_version: bool) -> str | None:
     """Find the correct version number to use for saving."""
-    existing_versions = [re.search(r"_v([^_]+)$", f).group(1) for f in files]
+    if None is in files:
+        return None
+    else:
+        existing_versions = [re.search(r"_v([^_]+)$", f).group(1) for f in files]
     print(existing_versions)
     if not stable_version and len(files) == 0:
         return "0"
