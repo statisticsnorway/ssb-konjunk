f2c70b3772bf66400756f9b195552a68406fb20d
leonard biermann <lbi@ssb.no>
2024-08-22 09:59:54 +0200

    endret feilmelding til norsk, lagt til filsti

--- src/ssb_konjunk/saving.py
+++ src/ssb_konjunk/saving.py
@@ -378,9 +378,10 @@ def read_ssb_file(
         files = _get_files(file_path, filetype, fs=fs)
         # If list is empty, no matching files of any version were found.
         if not files:
-            raise FileNotFoundError("Could not find any files matching format.")
+            raise FileNotFoundError(f"Fant ingen {filetype}-filer som matcher filstien '{file_path}'.")
         # Otherwise, use the newest version of file.
         file_path = files[-1]
+
     # Different functions used for reading depending on the filetype.
     if filetype == "csv":
         if fs:
