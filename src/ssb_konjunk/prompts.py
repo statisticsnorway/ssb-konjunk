2ae1f5bea91293e709ca6bfcd02b4ccfbd3b6bcc
Johanne Saxegaard <jox@ssb.no>
2024-08-28 15:17:08 +0200

    New bump_quarter function

--- src/ssb_konjunk/prompts.py
+++ src/ssb_konjunk/prompts.py
@@ -220,6 +220,27 @@ def iterate_years_months(
             ):
                 continue
             yield year, month
+            
+def bump_quarter(year: int, quarter: int) -> tuple[int, int]:
+    """Bump period with a quarter further.
+    
+    E.g. 2023 and quarter 4 as input, will be returned as 2024 and quarter 1.
+    
+    Args:
+        year: The year.
+        quarter: The quarter.
+    
+    Returns:
+        tuple: The year and quarter with an "added" quarter.
+    """
+    if quarter == 4:
+        bumped_quarter = 1
+        bumped_year = year + 1
+    else:
+        bumped_quarter = quarter + 1
+        bumped_year = year
+
+    return bumped_year, bumped_quarter
 
 
 def validate_month(month: int | str) -> str:
