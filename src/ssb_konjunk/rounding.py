17ab2ccb3513716b378f50a80bcb131670e239f1
Johanne Saxegaard <jox@ssb.no>
2024-09-19 14:53:16 +0200

    Added rounding func

---
+++ src/ssb_konjunk/rounding.py
@@ -0,0 +1,34 @@
+import math
+from decimal import *
+import pandas as pd
+
+def round_half_up_float(n:float, decimals:int = 0) -> float|int:
+    """Round a float half up.
+    
+    Function from https://realpython.com/python-rounding/.
+    
+    Args:
+        n: the float to round off.
+        decimals: the number of decimals to keep.
+    Returns:
+        float|int: the rounded off number. 
+    """
+    multiplier = 10**decimals
+    return math.floor(n * multiplier + 0.5) / multiplier
+
+def round_half_up(df_col: pd.Series, digits: str ='1.') -> pd.Series:
+    """Round a pandas column half up.
+    
+    The "normal" (half up) rounding should be used.
+
+    Args:
+        df_col: a column in a data frame where all values will be rounded off.
+        digits: number of digits after . gives the number of digits rounded off to. Default: no digits.
+    
+    Returns:
+        pd.Series: a column in a data frame where all values are rounded off
+    """
+    df_col = df_col.copy()
+    df_col = df_col.map(lambda x: Decimal(x).quantize(Decimal(digits), rounding=ROUND_HALF_UP))
+    
+    return df_col
