7cb5718e20b92bb3b09b62e49fa086f8075f0510
ssb-jox <jox@ssb.no>
2025-08-07 15:06:25 +0200

    ok

--- personal-init.sh
+++ personal-init.sh
@@ -4,6 +4,8 @@
 SETTINGS_DIR="$HOME/work/.jupyter/config/lab/user-settings/@jupyterlab/docmanager-extension"
 SETTINGS_FILE="$SETTINGS_DIR/plugin.jupyterlab-settings"
 
+mkdir -p "$SETTINGS_DIR"
+
 cat > "$SETTINGS_FILE" <<EOF
 {
     "autosave": false
