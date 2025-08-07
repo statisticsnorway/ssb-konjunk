1565aae039e3fd7d1acde96dfe39b5db4924d050
Johanne Saxegaard <jox@ssb.no>
2025-08-07 15:14:15 +0200

    Autosave (#99)
    * ok
    
    * ok
    
    * ok

--- personal-init.sh
+++ personal-init.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 # Set Jupyter autosave off
 SETTINGS_DIR="$HOME/work/.jupyter/config/lab/user-settings/@jupyterlab/docmanager-extension"
 SETTINGS_FILE="$SETTINGS_DIR/plugin.jupyterlab-settings"
-
+mkdir -p "$SETTINGS_DIR"
 cat > "$SETTINGS_FILE" <<EOF
 {
     "autosave": false
