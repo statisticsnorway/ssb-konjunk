cd0ba17e079b6cabbb5062f8d8dd1119e68e47fa
Johanne Saxegaard <jox@ssb.no>
2025-08-07 11:59:12 +0200

    Autosave off (#97)
    * Autosave off
    
    * nox

---
+++ personal-init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Set Jupyter autosave off
+SETTINGS_DIR="$HOME/work/.jupyter/config/lab/user-settings/@jupyterlab/docmanager-extension"
+SETTINGS_FILE="$SETTINGS_DIR/plugin.jupyterlab-settings"
+
+{
+    "autosave": false
+}
