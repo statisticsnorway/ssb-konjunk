ff24b3e95f181a86254d035e7973aeb29dc0db7a
ssb-hvr <hvr@ssb.no>
2026-02-12 10:59:36 +0100

    change in the savings file to fix the dapla-toolbelt bug

--- src/ssb_konjunk/saving.py
+++ src/ssb_konjunk/saving.py
@@ -11,6 +11,7 @@ import pandas as pd
 
 from ssb_konjunk import timestamp
 
+from gcsfs import GCSFileSystem
 
 def _remove_edge_slashes(input_string: str, only_last: bool = False) -> str:
     """Function to remove edge slashes in strings.
@@ -39,6 +40,7 @@ def _structure_ssb_filepath(
     undermappe: str | None = None,
     version_number: int | None = None,
     filetype: str = "parquet",
+    fs: GCSFileSystem | None = None,
 ) -> str:
     """Structure the name of the file to SSB-format and the path.
 
@@ -59,7 +61,10 @@ def _structure_ssb_filepath(
     Raises:
         ValueError: Raise if version number is not None or int.
     """
-    bucket = _remove_edge_slashes(bucket)
+    if fs is None:
+        bucket = _remove_edge_slashes(bucket, only_last=True)
+    else:
+        bucket = _remove_edge_slashes(bucket)
     kortnavn = _remove_edge_slashes(kortnavn)
     datatilstand = _remove_edge_slashes(datatilstand)
     file_name = _remove_edge_slashes(file_name)
@@ -89,12 +94,17 @@ def _structure_ssb_filepath(
     return file_path
 
 
-def _get_files(folder_path: str, filetype: str) -> list[str]:
+def _get_files(
+    folder_path: str, filetype: str, fs: GCSFileSystem | None = None
+) -> list[str]:
     """Function to list files in a folder based on base name and timestamp."""
     filenames = []
 
     match_string = f"{folder_path}*"
-    filenames = glob.glob(match_string)
+    if fs:
+        filenames = fs.glob(match_string)
+    else:
+        filenames = glob.glob(match_string)
 
     # Only include files with the relevant file extension
     filenames = [i for i in filenames if i.endswith(filetype)]
