76ea73997359eee75d0aaca8707a93748d998530
ssb-hvr <hvr@ssb.no>
2026-07-30 10:20:55 +0200

    removed test file

--- tests/dash/untitled.py
+++
@@ -1,8 +0,0 @@
-from ssb_konjunk.dash.calculations.calc_data import DataManager
-import pandas as pd
-
-test_series = pd.Series(["42.1", "40.2", "40", "F", "40.1"])
-sorted_series = DataManager.sort_aggregates(test_series).tolist()
-sorted_series
-
-
